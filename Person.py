40db767395d91fdaa5de0f4c97fd7716e97bd0d4
Rojan Maharjan <qn8676ik@mnstate.edu>
2016-05-04 22:43:06 -0500

    edited constructor

--- Person.py
+++ Person.py
@@ -1,7 +1,8 @@
 class Person:
 -lastIDUsed = 100
-def __init__(self, tmpFname, tmpLname, tmpID):
+def __init__(self, tmpFname, tmpLname):
 self._fName = tmpFname
 self._lName = tmpLname
-self._ID = tmpID
+self._ID = Person._lastIDUsed
+Person._lastIDUsed += 1
 
